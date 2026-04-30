cc50e45ec12c1cb5fb99f1176b37c43fc1714856
Geonomy Labs <dev@geonomylabs.com>
2026-04-30 17:37:25 -0500

    Replaced Plot with cairomakie in benchmarks and charts. Updated plots size for CairoMakie.

--- src/benchmarks/post_processing/analytical/SimpleSedimentationManager.jl
+++ src/benchmarks/post_processing/analytical/SimpleSedimentationManager.jl
@@ -187,7 +187,7 @@ function make_benchmark_plot(data::SimpleSedimentation)::Nothing
         "figure_dpi" => 150,
         "legend_location" => :bottomleft,
         "legendfontsize" => 12,
-        "figsize" => (10, 5),
+        "figsize" => (6, 4),
         "guidefontsize" => 12,
         "titlefontsize" => 15,
         "tickfontsize" => 10,
