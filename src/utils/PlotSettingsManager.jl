4da77ce1996494866e852bb26eba9d9271b6bafa
Geonomy Labs <dev@geonomylabs.com>
2026-04-30 14:56:57 -0500

    Suppressed terminal plot popup for benchmarks.

--- src/utils/PlotSettingsManager.jl
+++ src/utils/PlotSettingsManager.jl
@@ -6,6 +6,10 @@ Module for managing general plot settings.
 """
 module PlotSettingsManager
 
+function __init__()
+    get!(ENV, "GKSwstype", "100")
+end
+
 @Base.kwdef mutable struct PlotSettings
     plot_extension::String = ".png"
 end
